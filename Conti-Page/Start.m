5c8f4816896b4a4fe3c450b74191bf9a385b866f
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-05-12 08:55:56 -0500

    Page view controller configuration added

--- Conti-Page/Start.m
+++ Conti-Page/Start.m
@@ -7,21 +7,104 @@
 //
 
 #import "Start.h"
+#import "IntroPages.h"
+
+int     iNumberOfPages = 3;
 
 @interface Start ()
 
 @end
 
 @implementation Start
-
-- (void)viewDidLoad {
+/**********************************************************************************************/
+#pragma mark - Initialization
+/**********************************************************************************************/
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+/**********************************************************************************************/
+#pragma mark - Page controller functions and delagates
+/**********************************************************************************************/
+- (void)createPageViews
+{
+    // Create page view controller
+    self.pageViewController             = [self.storyboard instantiateViewControllerWithIdentifier:@"IntroPageController"];
+    self.pageViewController.dataSource  = self;
+    
+    IntroPages *startingViewController  = [self viewControllerAtIndex:0];
+    NSArray *viewControllers            = @[startingViewController];
+    [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    // Change the size of page view controller
+    self.pageViewController.view.frame  = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self addChildViewController:self.pageViewController];
+    [self.view addSubview:self.pageViewController.view];
+    [self.pageViewController didMoveToParentViewController:self];
+    
+    NSArray *subviews = self.pageViewController.view.subviews;
+    UIPageControl *thisControl = nil;
+    for (int i=0; i<[subviews count]; i++) {
+        if ([[subviews objectAtIndex:i] isKindOfClass:[UIPageControl class]]) {
+            thisControl = (UIPageControl *)[subviews objectAtIndex:i];
+        }
+    }
+    thisControl.hidden = true;
+}
+//-------------------------------------------------------------------------------
+- (IntroPages *)viewControllerAtIndex:(NSUInteger)index
+{
+    if ((iNumberOfPages == 0) || (index >= iNumberOfPages)) {
+        return nil;
+    }
+    
+    // Create a new view controller and pass suitable data.
+    IntroPages *pagePoolsTable     = [self.storyboard instantiateViewControllerWithIdentifier:@"IntroPages"];
+    pagePoolsTable.iPageIndex       = index;
+    NSLog(@"viewControllerAtIndex index = %d", (int)index);
+    return pagePoolsTable;
+}
+//-------------------------------------------------------------------------------
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
+{
+    NSUInteger index = ((IntroPages*) viewController).iPageIndex;
+    
+    if ((index == 0) || (index == NSNotFound)) {
+        
+        return nil;
+    }
+    
+    index--;
+    return [self viewControllerAtIndex:index];
+}
+//-------------------------------------------------------------------------------
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
+{
+    NSUInteger index = ((IntroPages*) viewController).iPageIndex;
+    
+    if (index == NSNotFound) {
+        return nil;
+    }
 
+    index++;
+    return [self viewControllerAtIndex:index];
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController
+{
+    return iNumberOfPages;
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController
+{
+    NSLog(@"presentationIndexForPageViewController");
+    return 0;
+}
 @end
